047181e66ecd1566a5d5d134220486ec9b8080ee
David Walton <david@mapletonhillmedia.com>
2015-01-21 21:03:47 -0700

    bubbles parallaxing & menu stuff

--- scripts/page.js
+++ scripts/page.js
@@ -9,31 +9,133 @@
 
     function init() {
         cacheDom();
+        addAtoms();
         attachHandlers();
-        handleScroll();
+        onScroll();
     }
 
     function attachHandlers() {
-        dom.window.on('scroll', handleScroll);
-        dom.window.on('resize', handleScroll);
+        dom.window.on('scroll', onScroll);
+        dom.window.on('resize', onResize);
+        $.each(dom.navElements, function (index, item) {
+            $(item).on('click', onNavElementClicked);
+        });
     }
 
     function cacheDom() {
         dom.window = $(window);
+        dom.htmlBody = $('html,body');
         dom.header = $('header');
+        dom.navElements = dom.header.find('nav a');
         dom.phone = $('.phone');
         dom.bringToLife = $('.bring-to-life');
         dom.subHead = dom.bringToLife.find('h2');
         dom.whatIsAgile = $('.what-is-agile');
         dom.whatIsAgileHeader = dom.whatIsAgile.find('h3');
         dom.whatIsAgileCopy = dom.whatIsAgile.find('p');
+        dom.atomsContainer = $('.atoms');
     }
 
-    function handleScroll(eventObject) {
+    function addAtoms() {
+        var i, rand, div, $div, isFuzzy;
+
+        dom.atoms = [];
+        dom.fuzzyAtoms = [];
+
+        for (i = 0; i < 10; i += 1) {
+            div = document.createElement('div');
+            $div = $(div);
+            $div.addClass('orb');
+
+            rand = Math.floor(Math.random() * 2);
+
+            if (rand > 0) {
+                $div.addClass('blue');
+            } else {
+                $div.addClass('green');
+            }
+
+            rand = Math.floor(Math.random() * 2);
+
+            if (rand > 0) {
+                $div.addClass('fuzzy');
+                isFuzzy = true;
+            } else {
+                isFuzzy = false;
+            }
+
+            rand = Math.floor(Math.random() * 2);
+
+            if (rand > 0) {
+                $div.addClass('reversed');
+            }
+
+            rand = Math.floor(Math.random() * 115) + 35;
+
+            $div.css({'height': rand + 'px', 'width': rand + 'px'});
+
+            rand = Math.floor(Math.random() * dom.window.width());
+
+            $div.css('left', rand + 'px');
+
+            rand = Math.floor(Math.random() * 1100) + 600;
+
+            $div.css('top', rand + 'px');
+            $div.data('top', rand);
+
+            if (isFuzzy) {
+                dom.fuzzyAtoms.push($div);
+            } else {
+                dom.atoms.push($div);
+            }
+
+            dom.atomsContainer.append($div);
+        }
+    }
+
+    function onNavElementClicked() {
+        var $this = $(this),
+            href = $this.attr('href'),
+            $destination = $(href);
+
+        dom.htmlBody.animate({
+            scrollTop: $destination.offset().top
+        }, 400);
+
+        return false;
+    }
+
+    function onResize() {
+        onScroll();
+    }
+
+    function onScroll(eventObject) {
         collapseOrExpandHeaderMenu();
         parallaxPhone();
         fadeSubHead();
         fadeWhatIsAgile();
+        parallaxAtoms();
+    }
+
+    function parallaxAtoms() {
+        var scrollTop = dom.window.scrollTop(),
+            atomsContainerOffset = dom.atomsContainer.offset().top,
+            windowHeight = dom.window.height(),
+            visibilityThreshold = atomsContainerOffset - windowHeight,
+            fuzzyOffset = (scrollTop - visibilityThreshold) / 4,
+            offset = (scrollTop - visibilityThreshold) / 2;
+
+        if (offset > 0) {
+            $.each(dom.atoms, function (index, item) {
+                item.css('top', (item.data('top') - offset) + 'px');
+            });
+
+            $.each(dom.fuzzyAtoms, function (index, item) {
+                item.css('top', (item.data('top') - fuzzyOffset) + 'px');
+            });
+
+        }
+
     }
 
     function fadeWhatIsAgile() {
