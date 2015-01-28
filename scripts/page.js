c10890bf41195d6dfbc50a083d1e3bf5a9e12352
David Walton <david@mapletonhillmedia.com>
2015-01-27 23:24:00 -0700

    done with mobile ish

--- scripts/page.js
+++ scripts/page.js
@@ -11,7 +11,7 @@
         cacheDom();
         addAtoms();
         attachHandlers();
-        onScroll();
+        onResize();
     }
 
     function attachHandlers() {
@@ -20,6 +20,7 @@
         $.each(dom.navElements, function (index, item) {
             $(item).on('click', onNavElementClicked);
         });
+        dom.h1Anchor.on('click', onH1AnchorClicked);
     }
 
     function cacheDom() {
@@ -27,17 +28,28 @@
         dom.htmlBody = $('html,body');
         dom.header = $('header');
         dom.navElements = dom.header.find('nav a');
+        dom.h1 = dom.header.find('h1');
+        dom.h1Anchor = dom.h1.find('a');
         dom.phone = $('.phone');
         dom.bringToLife = $('.bring-to-life');
         dom.subHead = dom.bringToLife.find('h2');
         dom.whatIsAgile = $('.what-is-agile');
+        dom.whatIsAgileInner = dom.whatIsAgile.find('div');
         dom.whatIsAgileHeader = dom.whatIsAgile.find('h3');
         dom.whatIsAgileCopy = dom.whatIsAgile.find('p');
         dom.atomsContainer = $('.atoms');
+        dom.technology = $('.technology');
+        dom.technologyCopy = dom.technology.find('p');
+        dom.technologyHeader = dom.technology.find('h3');
+        dom.services = $('.web-applications');
+        dom.servicesLogos = dom.services.find('ul');
+        dom.aboutUs = $('.about-us');
+        dom.aboutUsProfiles = dom.aboutUs.find('li');
+        dom.footer = $('footer');
     }
 
     function addAtoms() {
-        var i, rand, div, $div, isFuzzy;
+        var i, rand, div, $div, isFuzzy, width;
 
         dom.atoms = [];
         dom.fuzzyAtoms = [];
@@ -70,13 +82,16 @@
                 $div.addClass('reversed');
             }
 
-            rand = Math.floor(Math.random() * 115) + 35;
+            rand = Math.floor(Math.random() * 20) + 15;
 
-            $div.css({'height': rand + 'px', 'width': rand + 'px'});
+            width = rand;
 
-            rand = Math.floor(Math.random() * dom.window.width());
+            $div.css({'height': rand + '%', 'width': rand + '%'});
 
-            $div.css('left', rand + 'px');
+            //rand = Math.floor(Math.random() * dom.window.width());
+            rand = Math.floor(Math.random() * (100 - width));
+
+            $div.css('left', rand + '%');
 
             rand = Math.floor(Math.random() * 1100) + 600;
 
@@ -105,16 +120,61 @@
         return false;
     }
 
+    function onH1AnchorClicked() {
+        dom.htmlBody.animate({
+            scrollTop: 0
+        }, 400);
+
+        return false;
+    }
+
     function onResize() {
+        setMinimumHeights();
+        positionWhatDoesAgileMean();
         onScroll();
     }
 
     function onScroll(eventObject) {
         collapseOrExpandHeaderMenu();
         parallaxPhone();
+        //parallaxWhatIsAgile();
         fadeSubHead();
         fadeWhatIsAgile();
         parallaxAtoms();
+        fadeTechnology();
+        parallaxAndFadeServiceLogos();
+        parallaxAndAboutUs();
+    }
+
+    function positionWhatDoesAgileMean() {
+        var height = dom.window.height(),
+            ratio = (height / dom.window.width()) * 65,
+            marginTop = Math.floor(height * 0.1) + 100;
+
+        dom.whatIsAgileCopy.css('width', ratio + '%');
+        dom.whatIsAgileHeader.css('width', ratio + '%');
+        dom.whatIsAgileHeader.css('margin', marginTop + 'px auto 0')
+    }
+
+    function setMinimumHeights() {
+        var minHeight = dom.window.height();
+
+        // the phone area has a tiny bit of parallax & so we need the height to be a little larger than window
+        // height to account for the movement
+        dom.phone.css('min-height', (minHeight + Math.floor(minHeight * 0.1)) + 'px');
+
+        // the bring to life margin should be a little more than window height in order to push the triangle
+        // offscreen to match the collapsed state of the menu
+        dom.bringToLife.css('margin', (minHeight + 21) + 'px 0 0');
+
+        dom.whatIsAgile.css('min-height', minHeight + 'px');
+
+        dom.whatIsAgileInner.css('min-height', minHeight + 'px');
+        //dom.whatIsAgileInner.css('min-height', (minHeight + Math.floor(minHeight * 0.2)) + 'px');
+
+        // the footer has a 1px top-padding in order to force the h3 margin to apply *within* the containing
+        // div so we take that px off the min-height
+        dom.footer.css('min-height', (minHeight - 1) + 'px');
     }
 
     function parallaxAtoms() {
@@ -136,6 +196,79 @@
 
         }
 
+    }
+
+    function parallaxAndFadeServiceLogos() {
+        var scrollTop = dom.window.scrollTop(),
+            windowHeight = dom.window.height();
+
+        $.each(dom.servicesLogos, function (index, item) {
+            fadeItem(scrollTop, windowHeight, item);
+            parallaxItem(scrollTop, windowHeight, item);
+        });
+    }
+
+    function parallaxAndAboutUs() {
+        var scrollTop = dom.window.scrollTop(),
+            windowHeight = dom.window.height();
+
+        $.each(dom.aboutUsProfiles, function (index, item) {
+            fadeItem(scrollTop, windowHeight, item);
+            parallaxItem(scrollTop, windowHeight, item);
+        });
+    }
+
+    function fadeTechnology() {
+        var scrollTop = dom.window.scrollTop(),
+            windowHeight = dom.window.height();
+
+        $.each(dom.technologyCopy, function (index, item) {
+            fadeItem(scrollTop, windowHeight, item);
+        });
+
+        fadeItem(scrollTop, windowHeight, dom.technologyHeader);
+    }
+
+    function parallaxItem(scrollTop, windowHeight, item) {
+        var $item = $(item),
+            offset = $item.offset().top,
+            halfHeight = windowHeight / 2,
+            midway = offset - halfHeight,
+            top;
+
+        if (scrollTop >= midway) {
+            $item.css('top', '0');
+        } else {
+            top = (midway - scrollTop);
+
+            if (top > halfHeight) {
+                top = halfHeight;
+            }
+
+            $item.css('top', top + 'px');
+        }
+    }
+
+    function fadeItem(scrollTop, windowHeight, item) {
+        var $item = $(item),
+            offset = $item.offset().top,
+            midway = offset - (windowHeight / 2),
+            opacity;
+
+        if (scrollTop >= midway) {
+            $item.css('opacity', '1');
+        } else {
+            opacity = (midway - scrollTop);
+
+            if (opacity > 300) {
+                opacity = 0;
+            } else {
+                opacity = 300 - opacity;
+                opacity = opacity / 300;
+            }
+
+            $item.css('opacity', opacity);
+        }
     }
 
     function fadeWhatIsAgile() {
@@ -173,10 +306,10 @@
         if (scrollTop >= midway) {
             dom.subHead.css('opacity', '1');
         } else {
-            opacity = scrollTop / midway;
+            opacity = (scrollTop - 200) / midway;
 
-            if (opacity < 0.2) {
-                opacity = 0.2;
+            if (opacity < 0) {
+                opacity = 0;
             }
             dom.subHead.css('opacity', opacity);
         }
@@ -185,9 +318,10 @@
     function collapseOrExpandHeaderMenu() {
         if (dom.window.scrollTop() > 0) {
             dom.header.removeClass('collapsed');
+            dom.header.removeClass('big-head');
             dom.phone.removeClass('shaded');
         } else {
-            dom.header.addClass('collapsed');
+            dom.header.addClass('big-head');
             dom.phone.addClass('shaded');
         }
     }
@@ -199,6 +333,20 @@
             dom.phone.css('top', '-' + (scrollTop/3) + 'px');
         } else {
             dom.phone.css('top', '0');
+        }
+    }
+
+    function parallaxWhatIsAgile() {
+        var scrollTop = dom.window.scrollTop(),
+            whatIsAgileTop = dom.whatIsAgile.offset().top;
+
+        // subtract the window height so that we parallax once the section is in view
+        whatIsAgileTop -= dom.window.height();
+
+        if (scrollTop > whatIsAgileTop) {
+            dom.whatIsAgileInner.css('top', '-' + ((scrollTop - whatIsAgileTop) / 15) + 'px');
+        } else {
+            dom.whatIsAgileInner.css('top', '0');
         }
     }
 
